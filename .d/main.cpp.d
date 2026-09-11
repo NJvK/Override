72146b611c8a1516233ce16e89da26b223016925
NJvK <vonkrogh.natalie@gmail.com>
2026-09-11 13:35:08 -0700

    idk stuff for color sensor i think

--- .d/main.cpp.d
+++ .d/main.cpp.d
@@ -677,7 +677,7 @@ bin/main.cpp.o: src/main.cpp include/main.h include/api.h \
  include/fmt/args.h include/lemlib/logger/message.hpp \
  include/lemlib/logger/infoSink.hpp \
  include/lemlib/logger/telemetrySink.hpp include/pros/distance.hpp \
- include/pros/screen.hpp
+ include/pros/optical.hpp include/pros/screen.hpp
 include/main.h:
 include/api.h:
 include/pros/adi.h:
@@ -1440,4 +1440,5 @@ include/lemlib/logger/message.hpp:
 include/lemlib/logger/infoSink.hpp:
 include/lemlib/logger/telemetrySink.hpp:
 include/pros/distance.hpp:
+include/pros/optical.hpp:
 include/pros/screen.hpp:
